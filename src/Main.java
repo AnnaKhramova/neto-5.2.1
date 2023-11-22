a7fac11d626810ac51d59d5a80a72cd24646a71b
akhramova <xaa2128@mail.ru>
2023-11-23 00:40:55 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> result = new ArrayList<>();
+        for (Integer x : intList) {
+            if (x <= 0) continue;
+            if (x % 2 != 0) continue;
+            result.add(x);
+        }
+        Collections.sort(result);
+
+        for (Integer x : result) {
+            System.out.println(x);
+        }
+    }
+}
